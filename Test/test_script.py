f2ba700de814c3f074ec92bba15a75de13893365
Magdevich <stanislav.magdevich@ip-192-168-0-101.ec2.internal>
2021-06-18 12:07:27 +0300

    2021-06-18 12:07

---
+++ Test/test_script.py
@@ -0,0 +1,25 @@
+import itertools
+import pymysql as MySQLdb
+import os
+import pprint
+import datetime
+
+def git_push():
+	now = datetime.datetime.now()
+	now = str(now.strftime("%Y-%m-%d %H:%M"))
+	os.system("git add . ")
+	os.system("git commit -m '%s'" % now)
+	os.system("git push origin master ")
+if __name__=="__main__":
+	pp = pprint.PrettyPrinter(indent=1, width=1000)
+	db = MySQLdb.connect(host="terminal.dwh.slicetest.com", user="qa", password="1234", database="qatestsautomationdb")
+	cursor = db.cursor()
+	cursor.execute('select sql_ from qatestsautomationdb.query where id = 4276')
+	result = cursor.fetchall()
+	myString = ' || '.join(map(str, itertools.chain(*result)))
+	cursor.execute('select name from query where id = 4276')
+	file_full_name = cursor.fetchall()
+	myString2 = ' || '.join(map(str, itertools.chain(*file_full_name)))
+	c = open(myString2+'.sql',"w")
+	c.write(myString)
+	git_push()
