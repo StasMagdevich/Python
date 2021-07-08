a7c46eb2406b4395072a672b1b4822a4a1747882
Magdevich <stanislav.magdevich@ip-192-168-0-101.ec2.internal>
2021-07-08 12:20:28 +0300

    2021-07-08 12:20

---
+++ Test/sql_dqr_redshift_payment_method.sql
@@ -0,0 +1,1 @@
+update\\n  cds.order_total_modifier\\nset\\n  cds_update_time = sysdate\,\\n  payment_method = regexp_replace(\\n    payment_method\,\\n    '(.*[Cc]ard( [Pp]ayment [Ww]ith)?)[^a-z][Xx*-]*[0-9]{4\,}([ -][0-9]{4\,})*(.*)'\,\\n    '$1 ^Payment^$4'\\n  )\\nwhere\\n  payment_method ~ '.*[Cc]ard( [Pp]ayment [Ww]ith)?[^a-z][Xx*-]*[0-9]{4\,}.*'
