688c211557166191d45b660719117749118f55dd
Magdevich <stanislav.magdevich@ip-192-168-0-101.ec2.internal>
2021-07-08 12:29:19 +0300

    2021-07-08 12:29

--- Test/sql_dqr_redshift_item.sql
+++
@@ -1,1 +0,0 @@
-/*lz_pii_transaction_id-message-redshift*/\\nupdate\\n  cds.item\\nset\\n  description = REGEXP_REPLACE (\\n    description\,\\n    '(.*)(transaction id: .*)'\,\\n    '$1transaction id: ^transaction_id^)'\\n  )\,\\n  cds_update_time = sysdate\\nWHERE\\n  description ILIKE '%(transaction id:%'\\n\\n
